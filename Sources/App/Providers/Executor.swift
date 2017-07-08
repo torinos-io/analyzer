35a78a0064f9315698d347587a5c985494f5f2e1
yuta24 <yuta24@bivre.com>
2017-07-08 23:15:55 +0900

    Get information from Cartfile.resolved

--- Sources/App/Providers/Executor.swift
+++ Sources/App/Providers/Executor.swift
@@ -40,7 +40,18 @@ struct CocoapodsExecutor: ExecutorType {
 
 struct CarthageExecutor: ExecutorType {
     func handle(_ stream: String) throws -> ExecutorResult {
-        notImplement()
+        let lines = stream.components(separatedBy: "\n")
+        let value: [String: StructuredData] = lines.flatMap { (line) -> [String: Any]? in
+                let words = line.components(separatedBy: " ")
+                guard let key = words.second else { return nil }
+                guard let value = words.third else { return nil }
+                return [key: value]
+            }
+            .toDictionary()
+            .valueMap {
+                .string($0)
+            }
+        return ExecutorResult(json: JSON(.object(value)))
     }
 }
 
@@ -66,11 +77,14 @@ private extension Yaml {
         case .array(let value):
             return JSON(.array(value.map { $0.toJSON().wrapped }))
         case .dictionary(let value):
-            let object = value.enumerated().flatMap { (offset, element) -> [String: StructuredData]? in
-                guard let key = element.key.toString() else { return nil }
-                return [key: element.value.toJSON().wrapped]
-            }
-            return JSON(.object(object[0]))
+            let object: [String: StructuredData] = value
+                .enumerated()
+                .flatMap { (offset, element) -> [String: StructuredData]? in
+                    guard let key = element.key.toString() else { return nil }
+                    return [key: element.value.toJSON().wrapped]
+                }
+                .toDictionary()
+            return JSON(.object(object))
         }
     }
 
@@ -91,5 +105,48 @@ private extension Yaml {
         case .dictionary(_):
             return nil
         }
+    }
+}
+
+private extension Array {
+    var second: Element? {
+        guard self.count > 2 else { return nil }
+        return self[1]
+    }
+    var third: Element? {
+        guard self.count > 3 else { return nil }
+        return self[2]
+    }
+}
+
+private extension Array {
+    func toDictionary<Key, Value>() -> [Key: Value] {
+        let elements = self.flatMap { $0 as? [Key: Value] }
+        guard elements.count > 0 else { return [:] }
+        return elements.reduce([:], { (result, element) -> [Key: Value] in
+            var newValue = [Key: Value]()
+            element.enumerated().forEach {
+                newValue[$1.key] = $1.value
+            }
+            return result.merge(newValue)
+        })
+    }
+}
+
+private extension Dictionary {
+    func valueMap<T>(transform: @escaping (Value) -> T) -> [Key: T] {
+        var newValue = [Key: T]()
+        self.forEach {
+            newValue[$0.key] = transform($0.value)
+        }
+        return newValue
+    }
+
+    func merge(_ b: [Key: Value]) -> [Key: Value] {
+        var newValue = self
+        b.forEach {
+            newValue[$0] = $1
+        }
+        return newValue
     }
 }
