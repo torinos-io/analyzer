9af30a28a0eb663e397d0fbad70a077377e9dab1
yuta24 <yuta24@bivre.com>
2017-07-08 21:48:30 +0900

    Convert JSON

--- Sources/App/Providers/Executor.swift
+++ Sources/App/Providers/Executor.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Yaml
+import JSON
 
 struct ExecutorError: Error {
     enum Code: String {
@@ -10,7 +11,7 @@ struct ExecutorError: Error {
 }
 
 struct ExecutorResult {
-    let raw: [String: Any]
+    let json: JSON
 }
 
 protocol ExecutorType {
@@ -30,9 +31,10 @@ struct CocoapodsExecutor: ExecutorType {
             }
             }()
 
-        let dependencies = hash.dictionary
+        let json = hash.toJSON()
+        debugPrint(json)
 
-        return ExecutorResult()
+        return ExecutorResult(json: json)
     }
 }
 
@@ -45,5 +47,49 @@ struct CarthageExecutor: ExecutorType {
 struct XCProjectExecutor: ExecutorType {
     func handle(_ stream: String) throws -> ExecutorResult {
         notImplement()
+    }
+}
+
+private extension Yaml {
+    func toJSON() -> JSON {
+        switch self {
+        case .null:
+            return JSON(.null)
+        case .bool(let value):
+            return JSON(.bool(value))
+        case .int(let value):
+            return JSON(.number(.int(value)))
+        case .double(let value):
+            return JSON(.number(.double(value)))
+        case .string(let value):
+            return JSON(.string(value))
+        case .array(let value):
+            return JSON(.array(value.map { $0.toJSON().wrapped }))
+        case .dictionary(let value):
+            let object = value.enumerated().flatMap { (offset, element) -> [String: StructuredData]? in
+                guard let key = element.key.toString() else { return nil }
+                return [key: element.value.toJSON().wrapped]
+            }
+            return JSON(.object(object[0]))
+        }
+    }
+
+    func toString() -> String? {
+        switch self {
+        case .null:
+            return nil
+        case .bool(let value):
+            return value.string
+        case .int(let value):
+            return String(value)
+        case .double(let value):
+            return String(value)
+        case .string(let value):
+            return value
+        case .array(_):
+            return nil
+        case .dictionary(_):
+            return nil
+        }
     }
 }
